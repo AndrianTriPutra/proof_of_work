89b002dae04ed2e333e5d4a6c378e59c39e6873f
atp <andrian.putra@miota.io>
2024-05-30 21:33:36 +0700

    rm

--- pkg/utils/domain/block.go
+++ pkg/utils/domain/block.go
@@ -11,11 +11,6 @@ type Block struct {
 	Data   []*Data `json:"data"`
 }
 
-type BlockSerialized struct {
-	Key   string `json:"key"`
-	Value *Block `json:"value"`
-}
-
 func (b *Block) Hash() string {
 	m, _ := json.Marshal(b)
 	return fmt.Sprintf("%x", sha256.Sum256(m))
